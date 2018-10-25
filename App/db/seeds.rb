e5700dc984506d8faf3712ac82834398edc6eb50
Lodi2244 <elodieletellier1@gmail.com>
2018-10-25 21:51:15 +0200

    DogBnB done

--- App/db/seeds.rb
+++ App/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  city = City.create!(city_name: Faker::Address.city)
+end
+
+10.times do
+  dog = Dog.create!(name: Faker::Pokemon.name, city_id: rand(1..10))
+end
+
+10.times do
+  dogsitter = Dogsitter.create!(name: Faker::Name.name, city_id: rand(1..10))
+end
+
+10.times do
+  strolltime = Stroll.create!(date: "#{Faker::Date.forward(30)}", dog_id: rand(1..10), dogsitter_id: rand(1..10))
+end
